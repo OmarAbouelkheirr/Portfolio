acd54c4a01988917db4cc1ef1b412ce1c496843f
Omar Aboelkheir <177960935+OmarAbouelkheirr@users.noreply.github.com>
2026-08-24 16:36:12 +0300

    feat: provide language state across portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { HomePage } from './pages/HomePage.tsx'
 import { ProjectDetailPage } from './pages/ProjectDetailPage.tsx'
 import { ScrollToTop } from './components/ScrollToTop.tsx'
 import { CustomCursor } from './components/CustomCursor.tsx'
+import { LanguageProvider } from './i18n/LanguageContext.tsx'
 
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <CustomCursor />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/projects/:projectId" element={<ProjectDetailPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <LanguageProvider>
+      <BrowserRouter>
+        <ScrollToTop />
+        <CustomCursor />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects/:projectId" element={<ProjectDetailPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </LanguageProvider>
   )
 }
 
