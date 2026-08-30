cd76896374d61c146bbca90c172add74a6cde9c4
Omar Aboelkheir <177960935+OmarAbouelkheirr@users.noreply.github.com>
2026-08-30 20:22:17 +0300

    Use Omar Abouelkhier as primary display name

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import './index.css'
 import './styles/main.css'
 import './styles/localization.css'
 import App from './App.tsx'
+import { profile } from './data/portfolioData.ts'
+
+profile.name = 'Omar Abouelkhier'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
